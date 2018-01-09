ad7f6aabd63f826f5a4745ab58fd360fe414c46a
李波 <tolibo@qq.com>
2018-01-10 00:19:45 +0800

    fixed

--- bll/BBS.js
+++ bll/BBS.js
@@ -709,7 +709,7 @@ class BBS extends Base {
 
             info.title = self.getThreadSubject({
                 $: $,
-                threadTitle: config.prefixSubjectName || threadTitle
+                threadTitle: threadTitle
             });
 
             //不需要回复，则只爬第一页
