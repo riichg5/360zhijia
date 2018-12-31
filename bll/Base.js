17b314a3b06dbcd52d4c5c4fb777cfe13573c211
李波 <tolibo@qq.com>
2018-12-31 23:26:58 +0800

    添加请求header

--- bll/Base.js
+++ bll/Base.js
@@ -85,7 +85,7 @@ class Base {
 			"Cache-Control": "no-cache",
 			"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36"
 		};
-	},
+	}
 
 	loadJSON (opts) {
 		let self = this;
